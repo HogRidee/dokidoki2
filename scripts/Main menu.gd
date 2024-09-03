b4fcd6c84dc1c4e6abe450ef8e5642e4dd7b832f
HogRidee <iaraoz061102@gmail.com>
2024-09-03 08:54:47 -0500

    Add player basic template

--- scripts/Main menu.gd
+++ scripts/Main menu.gd
@@ -12,7 +12,7 @@ func _process(delta):
 
 
 func _on_start_pressed():
-	get_tree().change_scene_to_file("res://game.tscn")
+	get_tree().change_scene_to_file("res://scenes/game.tscn")
 
 
 func _on_quit_pressed():
