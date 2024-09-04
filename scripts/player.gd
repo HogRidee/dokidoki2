7f5dc88359acd6db98c826110900108a6680ccaf
HogRidee <iaraoz061102@gmail.com>
2024-09-03 20:09:43 -0500

    Add walking animation to player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 var SPEED = 300
+@onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -9,6 +10,8 @@ func _physics_process(delta):
 	input_vector = input_vector.normalized()
 	if input_vector:
 		velocity = input_vector * SPEED
+		animated_sprite.play("walk_down")
 	else:
 		velocity = input_vector
+		animated_sprite.play("idle")
 	move_and_slide()
